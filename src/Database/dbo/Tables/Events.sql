9e39d10ca859c0d7ea5aa7a0fc0135d8cce6175b
Trent <53923200+trentstauff@users.noreply.github.com>
2020-12-28 21:02:03 -0500

    events table

---
+++ src/Database/dbo/Tables/Events.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Events]
+(
+	[EventId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] NVARCHAR(450) NOT NULL, 
+    [StartDatetime] DATETIMEOFFSET NULL, 
+    [EndDatetime] DATETIMEOFFSET NOT NULL, 
+    [Title] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(1000) NULL, 
+    [Reminder] DATETIMEOFFSET NULL, 
+    [Priority] TINYINT NOT NULL, 
+    [TimeRequired] DATETIME2 NULL, 
+    [Difficulty] TINYINT NULL, 
+    [CreationDatetime] DATETIMEOFFSET NOT NULL, 
+    [ModificationDatetime] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [FK_Events_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id])
+)
