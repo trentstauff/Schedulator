2733c9712279e7d46600cb2ef60a1ca6580e4aa8
Trent <53923200+trentstauff@users.noreply.github.com>
2020-12-28 21:24:10 -0500

    Addition of Models and fixes to events table

---
+++ src/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
