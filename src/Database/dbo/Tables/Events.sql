6e316179887aec4b51e2d26098f8607d1adbdd65
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-01-05 22:04:29 -0500

    Import aspnetuser sql scripts

--- src/Database/dbo/Tables/Events.sql
+++ src/Database/dbo/Tables/Events.sql
@@ -1,16 +1,18 @@
-﻿CREATE TABLE [dbo].[Events]
-(
-	[EventId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [UserId] NVARCHAR(450) NOT NULL, 
-    [StartDatetime] DATETIMEOFFSET NULL, 
-    [EndDatetime] DATETIMEOFFSET NOT NULL, 
-    [Title] NVARCHAR(100) NOT NULL, 
-    [Description] NVARCHAR(1000) NULL, 
-    [Reminder] DATETIMEOFFSET NULL, 
-    [Priority] TINYINT NOT NULL, 
-    [TimeRequired] DATETIME2 NULL, 
-    [Difficulty] TINYINT NULL, 
-    [CreationDatetime] DATETIMEOFFSET NOT NULL, 
-    [ModificationDatetime] DATETIMEOFFSET NOT NULL, 
-    CONSTRAINT [FK_Events_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id])
-)
+﻿CREATE TABLE [dbo].[Events] (
+    [EventId]              BIGINT             IDENTITY (1, 1) NOT NULL,
+    [UserId]               NVARCHAR (450)     NOT NULL,
+    [StartDatetime]        DATETIMEOFFSET (7) NULL,
+    [EndDatetime]          DATETIMEOFFSET (7) NOT NULL,
+    [Title]                NVARCHAR (100)     NOT NULL,
+    [Description]          NVARCHAR (1000)    NULL,
+    [Reminder]             DATETIMEOFFSET (7) NULL,
+    [Priority]             TINYINT            NOT NULL,
+    [TimeRequired]         DATETIME2 (7)      NULL,
+    [Difficulty]           TINYINT            NULL,
+    [CreationDatetime]     DATETIMEOFFSET (7) NOT NULL,
+    [ModificationDatetime] DATETIMEOFFSET (7) NOT NULL,
+    PRIMARY KEY CLUSTERED ([EventId] ASC),
+    CONSTRAINT [FK_Events_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
+
---
+++ src/Database/dbo/Tables/PersistedGrants_1.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[PersistedGrants] (
+    [Key]          NVARCHAR (200) NOT NULL,
+    [Type]         NVARCHAR (50)  NOT NULL,
+    [SubjectId]    NVARCHAR (200) NULL,
+    [ClientId]     NVARCHAR (200) NOT NULL,
+    [CreationTime] DATETIME2 (7)  NOT NULL,
+    [Expiration]   DATETIME2 (7)  NULL,
+    [Data]         NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_PersistedGrants] PRIMARY KEY CLUSTERED ([Key] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_PersistedGrants_SubjectId_ClientId_Type]
+    ON [dbo].[PersistedGrants]([SubjectId] ASC, [ClientId] ASC, [Type] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_PersistedGrants_Expiration]
+    ON [dbo].[PersistedGrants]([Expiration] ASC);
+
