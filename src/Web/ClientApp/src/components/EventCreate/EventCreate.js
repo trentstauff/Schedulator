97f50f7da6c396d54d321e22f5e10fa66ed3a343
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-01-09 17:04:15 -0500

    Mostly finished event create

--- src/Web/ClientApp/src/components/EventCreate/EventCreate.js
+++ src/Web/ClientApp/src/components/EventCreate/EventCreate.js
@@ -1,13 +1,25 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import DateTime from "react-datetime";
+
+import "react-datetime/css/react-datetime.css";
+
+const MINUTES = 1;
+const HOURS = 2;
 
 const EventCreate = () => {
+  const [isTime, setIsTime] = useState(true);
+
   return (
-    <Form class="m-3">
+    <Form className="m-3">
+      <Form.Group controlId="formDatetime">
+        <Form.Label>Date and time</Form.Label>
+        <DateTime />
+      </Form.Group>
+
       <Form.Group controlId="formTitle">
         <Form.Label>Title</Form.Label>
         <Form.Control type="text" placeholder="Enter Title" />
-        <Form.Text className="text-muted">Testing 123</Form.Text>
       </Form.Group>
 
       <Form.Group controlId="formDescription">
@@ -15,26 +27,102 @@ const EventCreate = () => {
         <Form.Control as="textarea" placeholder="Description" />
       </Form.Group>
 
-      <Form.Group controlId="exampleForm.ControlSelect1">
-        <Form.Label>Priority</Form.Label>
+      <Form.Group controlId="formReminder">
+        <Form.Label>Reminder</Form.Label>
         <Form.Control as="select">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option>None</option>
+          <option>5 min</option>
+          <option>10 min</option>
+          <option>15 min</option>
+          <option>30 min</option>
+          <option>1 hr</option>
+          <option>2 hr</option>
+          <option>3 hr</option>
+          <option>6 hr</option>
+          <option>12 hr</option>
+          <option>42 hr</option>
         </Form.Control>
       </Form.Group>
 
-      <Form.Group controlId="formDescription">
-        <Form.Label>Time Required</Form.Label>
-        <Form.Control as="textarea" placeholder="Description" />
-      </Form.Group>
+      <Form.Label>Do you know the time required</Form.Label>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formisTimeRequired">
+          <Form.Label>Yes</Form.Label>
+          <Form.Control
+            type="radio"
+            defaultChecked={true}
+            value={true}
+            name="isTimeRequired"
+            onChange={(e) => setIsTime(true)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formDescription">
-        <Form.Label>Difficulty</Form.Label>
-        <Form.Control as="textarea" placeholder="Description" />
-      </Form.Group>
+        <Form.Group as={Col} controlId="formIsTimedRqueired">
+          <Form.Label>No</Form.Label>
+          <Form.Control
+            type="radio"
+            value={false}
+            name="isTimeRequired"
+            onChange={(e) => setIsTime(false)}
+          />
+        </Form.Group>
+      </Form.Row>
+
+      {isTime ? (
+        <>
+          <Form.Group controlId="formTimedRequired">
+            <Form.Label>Time Required</Form.Label>
+            <Form.Control type="number" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formTimedRequeiredUnit">
+              <Form.Label>Minutes</Form.Label>
+              <Form.Control
+                type="radio"
+                defaultChecked={true}
+                value={MINUTES}
+                name="units"
+                onChange={() => ""}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formTimedRqueiredUnit">
+              <Form.Label>Hours</Form.Label>
+              <Form.Control
+                type="radio"
+                value={HOURS}
+                name="units"
+                onChange={() => ""}
+              />
+            </Form.Group>
+          </Form.Row>
+        </>
+      ) : (
+        <>
+          <Form.Group controlId="formPriority">
+            <Form.Label>Priority</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="formDifficulty">
+            <Form.Label>Difficulty</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+        </>
+      )}
 
       <Button variant="primary" type="submit">
         Submit
